2fa62b17e8ee544a3f056b28dba9a03d64d3512f
alpais <lucia.pais@epfl.ch>
2016-09-13 10:09:11 +0200

    updated the scooping phases in the client script

--- bimanual-action-planners/scripts/scooping_demo.py
+++ bimanual-action-planners/scripts/scooping_demo.py
@@ -18,14 +18,25 @@ def query_init_attractors():
 	# Phase 0 ==== Initial Reach ===
 
 	# Phase 0 Right Arm Attractor in Task RF
+#	rA_p0_attr = geometry_msgs.msg.Transform()
+#	rA_p0_attr.translation.x = -0.296
+#	rA_p0_attr.translation.y =  0.197
+#	rA_p0_attr.translation.z =  0.345
+#	rA_p0_attr.rotation.x    =  0.358
+#	rA_p0_attr.rotation.y    =  0.010
+#	rA_p0_attr.rotation.z    =  0.166
+#	rA_p0_attr.rotation.w    =  0.919
+
+	# Phase 0 Right Arm Attractor in Task RF >> Right Arm reaches directly in the good pose
 	rA_p0_attr = geometry_msgs.msg.Transform()
-	rA_p0_attr.translation.x = -0.296
-	rA_p0_attr.translation.y =  0.197
-	rA_p0_attr.translation.z =  0.345
-	rA_p0_attr.rotation.x    =  0.358
-	rA_p0_attr.rotation.y    =  0.010
-	rA_p0_attr.rotation.z    =  0.166
-	rA_p0_attr.rotation.w    =  0.919
+	rA_p0_attr.translation.x = -0.1782
+	rA_p0_attr.translation.y = -0.0482
+	rA_p0_attr.translation.z =  0.2901
+	rA_p0_attr.rotation.x    =  0.8821
+	rA_p0_attr.rotation.y    =  0.3088
+	rA_p0_attr.rotation.z    =  0.0049
+	rA_p0_attr.rotation.w    =  0.3557
+
  
 	# Phase 0 Left Arm Attractor in Task RF
 	lA_p0_attr = geometry_msgs.msg.Transform()
@@ -39,84 +50,108 @@ def query_init_attractors():
 
 	return rA_p0_attr, lA_p0_attr
 
-def query_peeling_attractors():
+def query_scooping_attractors():
 	# Compute Attractors for Reach to Peel/Peel Action
 	# -> Read Master Robot Position and Compute Fixed Attractors				
 
-	# Phase 1 ==== Reach to Peel ===
+	# Phase 1 ==== Reach to Scoop ===
 	lA_p1_attr = geometry_msgs.msg.Transform()
-	lA_p1_attr.translation.x =  0.0963
-	lA_p1_attr.translation.y = -0.8266
-	lA_p1_attr.translation.z =  0.4928
-	lA_p1_attr.rotation.x    =  0.2591
-	lA_p1_attr.rotation.y    =  0.5997 
-	lA_p1_attr.rotation.z    = -0.7446
-	lA_p1_attr.rotation.w    =  0.1372
+	lA_p1_attr.translation.x = -0.0975
+	lA_p1_attr.translation.y = -1.4084
+	lA_p1_attr.translation.z =  0.4334
+	lA_p1_attr.rotation.x    =  0.4191
+	lA_p1_attr.rotation.y    = -0.5443
+	lA_p1_attr.rotation.z    =  0.5011
+	lA_p1_attr.rotation.w    =  0.5263
 
-	# Phase 2 ==== Peel ===
+	# Phase 2 ==== Scoop ===
 	lA_p2_attr = geometry_msgs.msg.Transform()
-	lA_p2_attr.translation.x =  0.1346
-	lA_p2_attr.translation.y = -0.8252	
-	lA_p2_attr.translation.z =  0.4950
-	lA_p2_attr.rotation.x    =  0.2587 
-	lA_p2_attr.rotation.y    = -0.6180
-	lA_p2_attr.rotation.z    =  0.6406
-	lA_p2_attr.rotation.w    =  0.3753
+	lA_p2_attr.translation.x = -0.1209
+	lA_p2_attr.translation.y = -1.4172
+	lA_p2_attr.translation.z =  0.3881
+	lA_p2_attr.rotation.x    =  0.0180
+	lA_p2_attr.rotation.y    = -0.2310
+	lA_p2_attr.rotation.z    =  0.7431
+	lA_p2_attr.rotation.w    =  0.6278
 
 	return lA_p1_attr,lA_p2_attr
 
 
-def query_rotate_attractors():
+def query_depart_attractors():
 	# Compute Attractors for Reach to Peel/Peel Action
 	# -> Read Master Robot Position and Compute Fixed Attractors				
 
-	# Phase 3 ==== Rotate and Reach ===
+	# Phase 3 ==== Depart and reach on top of the bowl ===
 	rA_p3_attr = geometry_msgs.msg.Transform()
-	rA_p3_attr.translation.x = -0.118
-	rA_p3_attr.translation.y =  0.080 
-	rA_p3_attr.translation.z =  0.248
-	rA_p3_attr.rotation.x    =  0.775 
-	rA_p3_attr.rotation.y    =  0.521 
-	rA_p3_attr.rotation.z    = -0.157             
-	rA_p3_attr.rotation.w    =  0.320
-
+	rA_p3_attr.translation.x = -0.2541
+	rA_p3_attr.translation.y =  0.0822
+	rA_p3_attr.translation.z =  0.3067
+	rA_p3_attr.rotation.x    =  0.9153
+	rA_p3_attr.rotation.y    =  0.3052
+	rA_p3_attr.rotation.z    =  0.0248
+	rA_p3_attr.rotation.w    =  0.2616
+ 
 	lA_p3_attr = geometry_msgs.msg.Transform()
-	lA_p3_attr.translation.x = -0.075 
-	lA_p3_attr.translation.y = -0.351   
-	lA_p3_attr.translation.z =  0.115
-	lA_p3_attr.rotation.x    =  0.066 
-	lA_p3_attr.rotation.y    =  0.851  
-	lA_p3_attr.rotation.z    =  0.518  
-	lA_p3_attr.rotation.w    = -0.051
+	lA_p3_attr.translation.x = -0.0959
+	lA_p3_attr.translation.y = -1.4167
+	lA_p3_attr.translation.z =  0.3794
+	lA_p3_attr.rotation.x    =  0.4350
+	lA_p3_attr.rotation.y    = -0.6174
+	lA_p3_attr.rotation.z    =  0.4472
+	lA_p3_attr.rotation.w    =  0.4791
 
 	return rA_p3_attr,lA_p3_attr	
 
 
-def query_retract_attractors():
-    # Phase 4 ==== Retract ===
+def query_trash_attractors():
+        # Phase 4 ==== Trash ===
 
 	# Phase 4 Right Arm Attractor in Task RF
 	rA_p4_attr = geometry_msgs.msg.Transform()
-	rA_p4_attr.translation.x =  0.0223
-	rA_p4_attr.translation.y = -0.3668	
-	rA_p4_attr.translation.z =  0.5264 
-	rA_p4_attr.rotation.x    =  0.8316 
-	rA_p4_attr.rotation.y    = -0.2546  
-	rA_p4_attr.rotation.z    = -0.0267
-	rA_p4_attr.rotation.w    = -0.4929  
-
-
+	rA_p4_attr.translation.x = -0.2606
+	rA_p4_attr.translation.y =  0.0957
+	rA_p4_attr.translation.z =  0.3134
+	rA_p4_attr.rotation.x    =  0.9179
+	rA_p4_attr.rotation.y    =  0.3059
+	rA_p4_attr.rotation.z    =  0.0257
+	rA_p4_attr.rotation.w    =  0.2513
+ 
 	# Phase 4 Left Arm Attractor in Task RF
 	lA_p4_attr = geometry_msgs.msg.Transform()
-	lA_p4_attr.translation.x =  0.0612
-	lA_p4_attr.translation.y =  -0.4058	
-	lA_p4_attr.translation.z = 0.6659
-	lA_p4_attr.rotation.x    =  0.2643
-	lA_p4_attr.rotation.y    =  -0.2694
-	lA_p4_attr.rotation.z    =  0.7338
-	lA_p4_attr.rotation.w    = 0.5650
+	lA_p4_attr.translation.x = -0.2770
+	lA_p4_attr.translation.y = -1.8095
+	lA_p4_attr.translation.z =  0.3791
+	lA_p4_attr.rotation.x    =  0.1115
+	lA_p4_attr.rotation.y    = -0.5058
+	lA_p4_attr.rotation.z    =  0.6715
+	lA_p4_attr.rotation.w    =  0.5299
 	
 	return rA_p4_attr, lA_p4_attr
+
+def query_retract_attractors():
+        # Phase 5 ==== Retract ===
+
+	# Phase 5 Right Arm Attractor in Task RF
+	rA_p5_attr = geometry_msgs.msg.Transform()
+	rA_p5_attr.translation.x = -0.2628
+	rA_p5_attr.translation.y =  0.1106
+	rA_p5_attr.translation.z =  0.3600
+	rA_p5_attr.rotation.x    =  0.9194
+	rA_p5_attr.rotation.y    =  0.3115
+	rA_p5_attr.rotation.z    =  0.0513
+	rA_p5_attr.rotation.w    =  0.2348
+ 
+	# Phase 5 Left Arm Attractor in Task RF
+	lA_p5_attr = geometry_msgs.msg.Transform()
+	lA_p5_attr.translation.x = -0.3672
+	lA_p5_attr.translation.y = -2.0300
+	lA_p5_attr.translation.z =  0.5603
+	lA_p5_attr.rotation.x    =  0.3275
+	lA_p5_attr.rotation.y    =  0.5751
+	lA_p5_attr.rotation.z    = -0.4075
+	lA_p5_attr.rotation.w    = -0.6293
+
+	return rA_p5_attr, lA_p5_attr
 
 def send_goal(action_type, phase, task_frame, right_attractor_frame, left_attractor_frame, timeout):
     print "Phase:", phase
@@ -173,10 +208,10 @@ def execute_peeling_planner():
 	while True:
 
 		print "\n\n= = = = = = = = = = = = = = = = = = = = = = = = = ="
-		raw_input('Press Enter to Run Reach-To-Peel with Coupled CDS')
+		raw_input('Press Enter to Run Reach-To-Scoop with Coupled CDS')
 		print "\n\n= = = = = = = = = = = = = = = = = = = = = = = = = ="
 
-		lA_p1_attr,lA_p2_attr = query_peeling_attractors()
+		lA_p1_attr,lA_p2_attr = query_scooping_attractors()
 
 		# Reach To Peel with Coupled CDS
 		action_type = 'COUPLED_LEARNED_MODEL'  
@@ -185,7 +220,7 @@ def execute_peeling_planner():
 		print result.success
 
 		print "\n\n= = = = = = = = = = = = = = = = = = ="
-		raw_input('Press Enter To Peel with Coupled CDS')
+		raw_input('Press Enter To Scoop with Coupled CDS')
 		print "\n\n= = = = = = = = = = = = = = = = = = ="
 
 		# Reach To Peel with Coupled CDS
@@ -194,15 +229,15 @@ def execute_peeling_planner():
 		print "Result:"		
 		print result.success
 
-		peel_var = raw_input("Do you want to peel again(any), rotate(r), or end(e)?")
+		peel_var = raw_input("Do you want to scoop again(any), depart(d), or end(e)?")
 
 		if peel_var == 'r':		
 			print "\n\n= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = "
 			raw_input('Press Enter to Run Bimanual ROTATE/REACH with Coordinated Reaching DS')
 			print "\n\n= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = "
 
-			# Rotate Master and Retract Slave
-			rA_p3_attr,lA_p3_attr = query_rotate_attractors()
+			# Master departs to make room for the Slave
+			rA_p3_attr,lA_p3_attr = query_depart_attractors()
 			result = send_goal(action_type, 'phase3', task_frame, rA_p3_attr, lA_p3_attr, 10)
 			print "Result:"		
 			print result.success			
@@ -211,17 +246,27 @@ def execute_peeling_planner():
 			break
 
 
+#	print "\n\n= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = ="
+#	raw_input('Press Enter to Run Bimanual TRASH with Coupled DS')
+#	print "\n\n= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = ="
+
+	# Trash with Decoupled DS
+#	rA_p4_attr, lA_p4_attr = query_trash_attractors()
+#	action_type = 'BIMANUAL_REACH'  
+#	result = send_goal(action_type, 'phase4',  task_frame, rA_p4_attr, lA_p4_attr, 10)
+#	print "Result:"
+#	print result.success
+
 	print "\n\n= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = ="
 	raw_input('Press Enter to Run Bimanual RETRACT with Coordinated Reaching DS')
 	print "\n\n= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = ="
 
-	# Reach with Decoupled DS
-	rA_p4_attr, lA_p4_attr = query_retract_attractors()
+	# Retract with Decoupled DS
+	rA_p5_attr, lA_p5_attr = query_retract_attractors()
 	action_type = 'BIMANUAL_REACH'  
-	result = send_goal(action_type, 'phase4',  task_frame, rA_p4_attr, lA_p4_attr, 10)
+	result = send_goal(action_type, 'phase4',  task_frame, rA_p5_attr, lA_p5_attr, 10)
 	print "Result:"
 	print result.success
-
 
 
 if __name__ == '__main__':
